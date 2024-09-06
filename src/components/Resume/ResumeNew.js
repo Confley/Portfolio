d262892d0ac7501669c8224ff472a9cfa3f7dab0
Confley <confleyesparza@gmail.com>
2024-09-05 20:08:35 -0600

    Update (add animation to preloader - add preloader to resume)

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,10 +6,12 @@ import pdf from "../../Assets/../Assets/C. V. Canvas.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import Preloader from "../Pre";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -17,14 +19,26 @@ function ResumeNew() {
 
   return (
     <div>
-      <Container fluid className="resume-section">
+      <Container
+        fluid
+        className="resume-section"
+      >
         <Particle />
+
+        <Preloader load={!load} />
+
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document
+            file={pdf}
+            className="d-flex justify-content-center"
+            onLoadSuccess={() => setLoad(true)}
+          >
+            <Page
+              pageNumber={1}
+              scale={width > 786 ? 1.7 : 0.6}
+            />
           </Document>
         </Row>
-        
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
